f3a341e49bd16e4a5f6f5c109498c56a0bd91df9
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-08 23:49:52 +0200

    my pokedex repository

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PokemonCardComponent from './components/PokemonCardComponent';
+
+class App extends React.Component {
+
+
+render() {
+  return (
+    <>
+<h1>Hello World</h1>
+<PokemonCardComponent/>
+</>
+  )
+}
+}
+export default App;
---
+++ src/components/PokemonCardComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+const PokemonCardComponent = () => {
+     
+    return <h1>Hello</h1>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default PokemonCardComponent;
