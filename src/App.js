ce1bd902cc9ee59f6782f9caa44e283747659d2c
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-09 19:25:12 +0200

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import PokemonCardComponent from './components/PokemonCardComponent';
+import React from 'react'
+import PokemonCardComponent from './components/PokemonCardComponent'
+import Navbar from './components/NavBar'
 
 class App extends React.Component {
-
-
-render() {
-  return (
-    <>
-<h1>Hello World</h1>
-<PokemonCardComponent/>
-</>
-  )
+  render() {
+    return (
+      <>
+        <Navbar />
+        <h1>Hello World</h1>
+        <PokemonCardComponent />f
+      </>
+    )
+  }
 }
-}
-export default App;
+export default App
