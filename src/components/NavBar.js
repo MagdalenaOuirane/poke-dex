49f721f89ce8c34cf08c9427a2036546cf6713c9
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-09 19:26:18 +0200

    create navigation

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react'
+import '../style/NavBar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <a class="navbar-brand">Pokedex</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
