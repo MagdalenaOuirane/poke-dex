d86b63b82a3c4f70226110c7d31b8ef11b2d98f2
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-16 22:26:06 +0200

    add props to component

--- src/components/PokemonCardComponent.js
+++ src/components/PokemonCardComponent.js
@@ -3,18 +3,22 @@ import React from 'react'
 import '../style/PokemonCardComponent.css'
 
 class PokemonCardComponent extends React.Component {
+  
   render() {
+    
     return (
       <>
         <div className="card">
           {/* <img className="card-img-top" 
-      src={this.props.url}
+      src={this.props.pokemonDetails && this.props.pokemonDetails.url}
        alt="Card image cap" /> */}
           <div className="header">
+          <h5 className="card-title" style={{textAlign:'left'}}>{this.props.pokemonDetails && this.props.pokemonDetails.id}</h5>
             <h5 className="card-title" style={{ textAlign: 'center' }}>
-              {this.props.name} : {this.props.url}
+              {this.props.pokemonDetails && this.props.pokemonDetails.name}
             </h5>
-            <p className="card-text">{this.props.pokemon}</p>
+      
+            {/* {this.props.pokemonDetails.effect_entries.map(effect_entry => <h5 className="card-text"> Effect : {effect_entry.effect}</h5>)} */}
           </div>
         </div>
       </>
