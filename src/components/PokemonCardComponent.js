06d5f1e1a1e1920b46e5588abe0ced0d2ee1bb17
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-16 13:46:51 +0200

    add component card

--- src/components/PokemonCardComponent.js
+++ src/components/PokemonCardComponent.js
@@ -1,27 +1,24 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react'
+import '../style/PokemonCardComponent.css'
 
-
-
-const PokemonCardComponent = () => {
-     
-    return <h1>Hello</h1>
+class PokemonCardComponent extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="card">
+          {/* <img className="card-img-top" 
+      src={this.props.url}
+       alt="Card image cap" /> */}
+          <div className="header">
+            <h5 className="card-title" style={{ textAlign: 'center' }}>
+              {this.props.name} : {this.props.url}{' '}
+            </h5>
+            <p className="card-text">{this.props.pokemon}</p>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default PokemonCardComponent;
+export default PokemonCardComponent
