3f892d783d73bf5da7ecf19d5e734880807a7593
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-04-16 22:30:14 +0200

    add funcionality for pagnation,fetchPokemonData()

--- src/components/ListOfPokemons.js
+++ src/components/ListOfPokemons.js
@@ -1,98 +1,85 @@
 import React from 'react'
 
-
 import PokemonCardComponent from './PokemonCardComponent'
 
 class ListOfPokemons extends React.Component {
-  
   state = {
-     
     pokemonList: [],
-    pokemonData : [],
+    pokemonData: [],
     pageNumber: 0,
-    pageItems: 20
+    pageItems: 20,
   }
 
-  // componentDidMount() {
-  //   let url= 'https://pokeapi.co/api/v2/pokemon/'
-  //   axios.get(url)
-  //   .then(response => {
-  //     let pokemonList = response.data.results
-  //     console.log('PokemonList:', pokemonList)
-  //     this.setState({
-  //       pokemonList: pokemonList,
-  //     })
-  //   })
-  // }
+  goToNextPage() {
+    return this.state.pageNumber + this.state.pageItems
+  }
+
+  handleClick = event => {
+    const newPage = this.goToNextPage()
+    this.setState(
+      {
+        newPage: this.state.pageNumber,
+      },
+      () => {
+        console.log('pageNumber' + this.state.pageNumber)
+        this.fetchPokemonData()
+
+      },
+    )
+  }
 
   componentDidMount() {
-    this.fetchPokemonData();
+    this.fetchPokemonData()
   }
-
 
   fetchPokemonData() {
-    let urlPage = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=' + this.state.pageNumber + '&limit' + this.state.pageItems;
+    let urlPage =
+      'https://pokeapi.co/api/v2/ability/?limit=20&offset=' +
+      this.state.pageNumber +
+      '&limit' +
+      this.state.pageItems
     fetch(urlPage)
       .then(response => response.json())
-       .then(data => {
-         if(data) {
-          this.setState({
-                  pokemonList : data.results },
-                  
-                  () => {
-                  
-                    this.state.pokemonList.map(pokemon => {
-                      fetch(pokemon.url)
-                      .then( response => response.json())
-                      .then (data => {
-                        if (data) {
-                          let urlInfo = this.state.pokemonData
-                          urlInfo.push(data)
-                          this.setState ({
-                            pokemonData: urlInfo
-                          })
-                        }
+      .then(data => {
+        if (data) {
+          this.setState(
+            {
+              pokemonList: data.results,
+              pokemonData: [],
+            },
+
+            () => {
+              this.state.pokemonList.map(pokemon => {
+                fetch(pokemon.url)
+                  .then(response => response.json())
+                  .then(data => {
+                    if (data) {
+                      this.setState({
+                        pokemonData: [...this.state.pokemonData, data],
                       })
-                      .catch(console.log)
-                    })
+                    }
                   })
-                }
+                  .catch(console.log)
               })
-              .catch(console.log)
+            },
+          )
+        }
+      })
+      .catch(console.log)
   }
 
-
-  
-
-
-
   render() {
-
-   
-
-
-
     return (
       <div className="row">
-        {/* {this.state.pokemonList.map(pokemon => ( 
-         <PokemonCardComponent
-         key={pokemon.name}
-         name={pokemon.name}
-         url={pokemon.url}
-         
-         />
-         ))}  */}
-         {this.state.pokemonList.map((pokemon,index) => (
-           <PokemonCardComponent
-           key={pokemon.name}
-           name={pokemon.name}
-           pokemon={this.state.PokemonData[index]}
-           />
-         ))
-         }
-       
+        {this.state.pokemonList.map((pokemon, index) => (
+          <PokemonCardComponent
+            key={pokemon.name}
+            name={pokemon.name}
+            pokemonDetails={this.state.pokemonData[index]}
+          />
+        ))}
       </div>
-     )
+    )
   }
 }
 
